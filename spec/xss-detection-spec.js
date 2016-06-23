bf9e3af35de390a43df9d3c242a6d60f7a3b4451
akos.sereg <akos.sereg@gmail.com>
2016-06-23 22:50:44 +0200

    Adding tests

---
+++ spec/xss-detection-spec.js
@@ -0,0 +1,84 @@
+var expressDefend = null; // System under test
+
+var responseMock = { 
+	status: function(statusCode) { 
+		this.statusCode = statusCode; 
+		return { send: function() {} };
+	} 
+};
+
+// Mocking helpers
+var nextMockInvoked = false;
+var nextMock = function() { nextMockInvoked = true; };
+
+function getMaliciousRequest() {
+	return { 
+		originalUrl: '/?page.html?name=<script>alert("hello world")</script>', 
+		headers: { }, 
+		connection: { remoteAddress: '127.0.0.1' }
+	};
+}
+
+describe("Path Traversal", function() {
+ 
+  beforeEach(function() { 
+  	nextMockInvoked = false;
+
+  	// Reset system-under-test
+  	expressDefend = require('../index');
+  	expressDefend.blacklistCandidates = [];
+  });
+
+  it('is detected', function() {
+
+  	// Arrange
+	var interceptor = expressDefend.protect({ consoleLogging: false, dropSuspiciousRequest: true, maxAttempts: 1 });
+	var request = getMaliciousRequest();
+	request.originalUrl = '/?page.html?name=../../etc/passwd';
+
+	// Act
+    interceptor(request, responseMock, nextMock);
+
+    // Assert
+	expect(nextMockInvoked).toBe(false);
+  });
+});
+
+describe("Cross Site Scripting", function() {
+ 
+  beforeEach(function() { 
+  	nextMockInvoked = false;
+
+  	// Reset system-under-test
+  	expressDefend = require('../index');
+  	expressDefend.blacklistCandidates = [];
+  });
+
+  it('is detected', function() {
+
+  	// Arrange
+	var interceptor = expressDefend.protect({ consoleLogging: false, dropSuspiciousRequest: true, maxAttempts: 1 });
+	var request = getMaliciousRequest();
+
+	// Act
+    interceptor(request, responseMock, nextMock);
+
+    // Assert
+	expect(nextMockInvoked).toBe(false);
+  });
+
+  it('is detected (case insensitively)', function() {
+
+  	// Arrange
+	var interceptor = expressDefend.protect({ consoleLogging: false, dropSuspiciousRequest: true, maxAttempts: 1 });
+	var request = getMaliciousRequest();
+	request.originalUrl = '/?page.html?name=<ScRiPt';
+
+	// Act
+    interceptor(request, responseMock, nextMock);
+
+    // Assert
+	expect(nextMockInvoked).toBe(false);
+  });
+
+});
